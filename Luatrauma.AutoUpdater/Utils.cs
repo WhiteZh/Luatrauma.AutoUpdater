d4d400629f49de70c6d5948eb50e44f44fd972f2
John Bonjour <whitewolfzhang@gmail.com>
2025-12-10 04:56:36 +0800

    created utility purpose static class Utils

---
+++ Luatrauma.AutoUpdater/Utils.cs
@@ -0,0 +1,13 @@
+﻿namespace Luatrauma.AutoUpdater;
+
+public static class Utils
+{
+    public static HttpClient CreateProductSpecificHttpClient()
+    {
+        var httpClient = new HttpClient();
+        
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Luatrauma.AutoUpdater/1.0");
+
+        return httpClient;
+    }
+}
