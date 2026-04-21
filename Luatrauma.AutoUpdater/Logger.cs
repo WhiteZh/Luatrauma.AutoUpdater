aba0a12a905862a0719400683e72c5d29fba2418
John Bonjour <whitewolfzhang@gmail.com>
2026-04-21 21:31:19 +0800

    better log

--- Luatrauma.AutoUpdater/Logger.cs
+++ Luatrauma.AutoUpdater/Logger.cs
@@ -10,11 +10,13 @@ namespace Luatrauma.AutoUpdater
     {
         public static void Log(string message, ConsoleColor color = ConsoleColor.Gray)
         {
+            string logEntry = $"[{DateTime.Now:O}] {message}";
+            
             Console.ForegroundColor = color;
-            Console.WriteLine(message);
+            Console.WriteLine(logEntry);
             Console.ForegroundColor = ConsoleColor.Gray;
 
-            File.AppendAllText("Luatrauma.AutoUpdater.Temp/log.txt", message + Environment.NewLine);
+            File.AppendAllText("Luatrauma.AutoUpdater.Temp/log.txt", logEntry + Environment.NewLine);
         }
     }
 }
